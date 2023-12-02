cdecb4b34e65215ea0618c0544b442151679e22e
wesley.mizerski <wesley.mizerski@conceptu.ind.br>
2023-12-02 19:02:41 -0300

    json folder

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,15 +1,18 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import enUS from "./en-US.json";
-import ptBR from "./pt-BR.json";
+import enUS from "./json/en-US.json";
+import ptBR from "./json/pt-BR.json";
 
 i18n.use(initReactI18next).init({
-    resources: {
-        "pt-BR": ptBR,
-        "en-US": enUS,
-    },
-    lng: "pt-BR",
-    interpolation: {
-        escapeValue: false,
-    },
+  resources: {
+    "pt-BR": ptBR,
+    "en-US": enUS,
+  },
+  lng: "pt-BR",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
 });
+
+export default i18n;
