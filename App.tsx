08d90d4ec839df4ac80058001190d866da328470
Guilherme Alves <guiloliveira.alves@gmail.com>
2023-12-02 12:13:46 -0300

    move files to pages

--- App.tsx
+++ App.tsx
@@ -1,16 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import * as React from "react";
-import { Text, View } from "react-native";
-import HomeScreen from "./src/Home";
-
-function SettingsScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Settings!</Text>
-        </View>
-    );
-}
+import { HomeScreen } from "./src/Pages/Home";
+import { SettingsScreen } from "./src/Pages/Settings";
 
 const Tab = createBottomTabNavigator();
 
