6f8bc630d09de4fc23086052629fa235e940207a
wesley.mizerski <wesley.mizerski@conceptu.ind.br>
2023-12-02 10:38:46 -0300

    Teste do Husky

--- src/Home.tsx
+++ src/Home.tsx
@@ -25,7 +25,7 @@ export default function HomeScreen() {
       setExistingData(response.data);
       console.log(response.data);
     } catch (error) {
-      console.log(error.response.data);
+      // console.log(error.response.data);
     }
   };
   useEffect(() => {
