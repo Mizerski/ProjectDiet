ff893a9740aff506f4d382121135ddfde7874bff
wesley.mizerski <wesley.mizerski@conceptu.ind.br>
2023-12-02 10:53:41 -0300

    Teste do Husky

---
+++ check-dependencies.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+
+const dependencies = {
+  ...packageJson.dependencies,
+  ...packageJson.devDependencies,
+};
+
+for (const [name, version] of Object.entries(dependencies)) {
+  if (version.startsWith("^") || version.startsWith("~")) {
+    console.error(
+      `A dependência "${name}" não está usando uma versão fixa: "${version}"`
+    );
+    process.exit(1);
+  }
+}
